babfa7175a74b1a1fd81f783953736b3ea722fa4
TataStrive <makchamp1@gmail.com>
2019-05-31 11:12:06 +0530

    Initial commit

---
+++ app/src/main/java/manish/khullar/namastegast/MainActivity.java
@@ -0,0 +1,26 @@
+package manish.khullar.namastegast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        imageView=findViewById(R.id.imageView);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.animation);
+        imageView.startAnimation(animation);
+
+    }
+}
